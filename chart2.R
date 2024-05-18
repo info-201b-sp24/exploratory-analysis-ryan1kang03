01a0a4f67a5e339b00d723693485744c55dc21c0
tyjnn <tyjn@uw.edu>
2024-05-17 21:21:01 -0700

    remove wd and change data to use xlsx

--- chart2.R
+++ chart2.R
@@ -11,9 +11,9 @@ library(stringr)
 library(ggplot2)
 
 
-setwd("/Users/annabellefallstrom/Desktop/INFO201/SCRIPTS-2/DATA")
+#setwd("/Users/annabellefallstrom/Desktop/INFO201/SCRIPTS-2/DATA")
 
-climate_change <- read_tsv("D_FINAl.tsv")
+climate_change <- read_excel("D_FINAL.xlsx")
 
 
   names(climate_change)[names(climate_change) == "Emissions Quality Flag (CDP)"] <- "emissions_quality_flag_cdp"
