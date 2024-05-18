9fe64df2f6c94c5cc761baf3f4b8d60a71a7f0e8
tyjnn <tyjn@uw.edu>
2024-05-17 23:29:54 -0700

    added summary file

---
+++ summary.R
@@ -0,0 +1,40 @@
+# libraries
+library("readxl")
+library("dplyr")
+library("ggplot2")
+
+# load data
+data <- read_excel("./D_FINAL.xlsx")
+
+# value 1: top emissions producing countries
+by_countries <- data %>%
+  group_by(`Country`) %>%
+  summarise(`Total emissions (CDP) [tCO2-eq]` = sum(`Total emissions (CDP) [tCO2-eq]`, na.rm = TRUE)) %>%
+  filter(`Total emissions (CDP) [tCO2-eq]` != 0) %>%
+  arrange(desc(`Total emissions (CDP) [tCO2-eq]`))
+
+# value 2: top emissions producing regions
+by_regions <- data %>%
+  group_by(`Region`) %>%
+  summarise(`Total emissions (CDP) [tCO2-eq]` = sum(`Total emissions (CDP) [tCO2-eq]`, na.rm = TRUE)) %>%
+  filter(`Total emissions (CDP) [tCO2-eq]` != 0) %>%
+  arrange(desc(`Total emissions (CDP) [tCO2-eq]`))
+
+# value 3: most commonly counted gasses
+gas_occurances <- data %>%
+  count(`Gases included (CDP)`) %>%
+  arrange(desc(n))
+
+# value 4: average emissions for all groups of included gases
+gas_avg <- data %>%
+  group_by(`Gases included (CDP)`) %>%
+  summarise(`Average emissions (CDP) [tCO2-wq]` = mean(`Total emissions (CDP) [tCO2-eq]`, na.rm = TRUE)) %>%
+  arrange(desc(`Average emissions (CDP) [tCO2-wq]`))
+
+# value 5: highest emissions / population ratio
+high_ratio <- data %>%
+  arrange(`Total emissions (CDP) [tCO2-eq]` / `Population (CDP)`) %>%
+  mutate(`Location` = paste(`City name`, ", ", `Country`, sep = "")) %>%
+  select(Location) %>%
+  tail(1) %>%
+  pull()
